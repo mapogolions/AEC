1963057e27fefbe8e8f1d6de93460324c96d59ee
Pavel <ivanovpavelalex45@gmail.com>
2019-07-10 20:35:24 +0500

    Added initial test

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const PROJECT_NAME = 'AEC';
+
+module.exports = PROJECT_NAME;
---
+++ index.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const PROJECT_NAME = require('./index.js');
+
+test('Initial test', () => {
+  expect(PROJECT_NAME).toBe('AEC');
+});
