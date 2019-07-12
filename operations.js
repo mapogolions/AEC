477d18b11324f791666a61993e57a6444a02223a
Pavel <ivanovpavelalex45@gmail.com>
2019-07-12 21:28:48 +0500

    Defined basic operations

---
+++ operations.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const PLUS = Symbol('PLUS');
+const MINUS = Symbol('MINUS');
+const TIMES = Symbol('TIMES');
+const DIV = Symbol('DIV');
+
+module.exports = { PLUS, MINUS, TIMES, DIV };
