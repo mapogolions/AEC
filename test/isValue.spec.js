1c6982744c965908229d4244bebf9365b3af02b3
Pavel <ivanovpavelalex45@gmail.com>
2019-07-13 12:56:28 +0500

    Added additional check for isValue func

--- test/isValue.spec.js
+++ test/isValue.spec.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const isValue = require('../src/isValue');
-const { Literal, Var } = require('../src/expressions');
+const { Literal, Var, Let, Operation } = require('../src/expressions');
 
-test('Should return true when the expression is a int literal', () => {
+test('Should return true when the expression is an int literal', () => {
   const expr = new Literal(10);
   expect(isValue(expr)).toBe(true);
 });
@@ -20,5 +20,15 @@ test('Should return true when the expression is a logic literal', () => {
 
 test('Should return false when the expression is a variable', () => {
   const expr = new Var('x');
+  expect(isValue(expr)).toBe(false);
+});
+
+test('Should return false when the expression is an operation', () => {
+  const expr = new Operation('expr', 'operation', 'expr');
+  expect(isValue(expr)).toBe(false);
+});
+
+test('Should return false when the expression is a let', () => {
+  const expr = new Let('x', 'expr', 'expr');
   expect(isValue(expr)).toBe(false);
 });
