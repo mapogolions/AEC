c3e561a6be5510f16c36c746e8ced064d22bc47d
Pavel <ivanovpavelalex45@gmail.com>
2019-07-13 14:56:22 +0500

    Tested of reduction a nested complicated operation expressions

--- test/evaluate.spec.js
+++ test/evaluate.spec.js
@@ -41,3 +41,40 @@ test('Should return the reduced literal when operation doing over literals', () 
     expect(evaluate(expr)).toEqual(expected);
   });
 });
+
+test('Should reduction a nested operation expressions', () => {
+  const testCases = [
+    {
+      left: new Operation(new Literal(1), ADD, new Literal(2)),
+      right: new Literal(2),
+      op: ADD,
+      expected: new Literal(5),
+    },
+    {
+      left: new Literal(-3),
+      right: new Operation(new Literal(2), SUB, new Literal(4)),
+      op: MUL,
+      expected: new Literal(6),
+    },
+    {
+      left: new Operation(new Literal(9), ADD, new Literal(1)),
+      right: new Operation(new Literal(100), DIV, new Literal(50)),
+      op: DIV,
+      expected: new Literal(5),
+    },
+    {
+      left: new Operation(
+        new Literal(2),
+        ADD,
+        new Operation(new Literal(2), MUL, new Literal(3)), // 6
+      ), // 2 + 6 = 8
+      right: new Operation(new Literal(1), SUB, new Literal(-1)), // 2
+      op: SUB,
+      expected: new Literal(6),
+    },
+  ];
+  testCases.forEach(({ left, op, right, expected }) => {
+    const expr = new Operation(left, op, right);
+    expect(evaluate(expr)).toEqual(expected);
+  });
+});
