f1cd8f6c734be8e4b42da3cd0c3ebcfcd8ac40ea
Pavel <ivanovpavelalex45@gmail.com>
2019-07-23 23:42:05 +0500

    Refactoring: created separate function for each substituion case

--- src/substitute.js
+++ src/substitute.js
@@ -9,36 +9,38 @@ const {
   isFunCall,
 } = require('./is');
 
+const subIntoOperation = (value, name, expr) => {
+  const { leftExpr, op, rightExpr } = expr;
+  return new Operation(
+    substitute(value, name, leftExpr),
+    op,
+    substitute(value, name, rightExpr),
+  );
+};
+
+const subIntoLetIn = (value, name, expr) => {
+  return new Let(
+    expr.name,
+    substitute(value, name, expr.headExpr),
+    // shadow
+    expr.name === name ? expr.bodyExpr : substitute(value, name, expr.bodyExpr),
+  );
+};
+
+const subIntoFunCall = (value, name, expr) => {
+  const { funExpr, argExpr } = expr;
+  return new FunCall(
+    substitute(value, name, funExpr),
+    substitute(value, name, argExpr),
+  );
+};
+
 const substitute = (value, name, expr) => {
   if (isLiteral(expr)) return expr;
-  if (isVariable(expr) && expr.name === name) {
-    return value;
-  }
-  if (isOperation(expr)) {
-    const { leftExpr, op, rightExpr } = expr;
-    return new Operation(
-      substitute(value, name, leftExpr),
-      op,
-      substitute(value, name, rightExpr),
-    );
-  }
-  if (isLetIn(expr)) {
-    return new Let(
-      expr.name,
-      substitute(value, name, expr.headExpr),
-      // shadow
-      expr.name === name
-        ? expr.bodyExpr
-        : substitute(value, name, expr.bodyExpr),
-    );
-  }
-  if (isFunCall(expr)) {
-    const { funExpr, argExpr } = expr;
-    return new FunCall(
-      substitute(value, name, funExpr),
-      substitute(value, name, argExpr),
-    );
-  }
+  if (isVariable(expr) && expr.name === name) return value;
+  if (isOperation(expr)) return subIntoOperation(value, name, expr);
+  if (isLetIn(expr)) return subIntoLetIn(value, name, expr);
+  if (isFunCall(expr)) return subIntoFunCall(value, name, expr);
   return expr;
 };
 
