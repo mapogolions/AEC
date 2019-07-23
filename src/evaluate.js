fd2a98449f183862744a08b5adfa745d9ffb5de3
Pavel <ivanovpavelalex45@gmail.com>
2019-07-23 19:07:17 +0500

    Refactoring: created separate function for each evaluable entity

--- src/evaluate.js
+++ src/evaluate.js
@@ -5,28 +5,32 @@ const substitute = require('./substitute');
 const { Fun, FunCall } = require('./expressions');
 const { isLetIn, isVariable, isOperation, isFun, isFunCall } = require('./is');
 
+const evalOperation = expr => {
+  const { leftExpr, op, rightExpr } = expr;
+  const leftValue = evaluate(leftExpr);
+  const rightValue = evaluate(rightExpr);
+  return apply(op, leftValue, rightValue);
+};
+
+const evalLetIn = expr => {
+  const { name, headExpr, bodyExpr } = expr;
+  return evaluate(new FunCall(new Fun(name, bodyExpr), headExpr));
+};
+
+const evalFunCall = expr => {
+  const { funExpr, argExpr } = expr;
+  const fn = evaluate(funExpr);
+  if (!isFun(fn)) throw new TypeError();
+  const { param, bodyExpr } = fn;
+  const value = evaluate(argExpr);
+  return evaluate(substitute(value, param, bodyExpr));
+};
+
 const evaluate = expr => {
-  if (isOperation(expr)) {
-    const { leftExpr, op, rightExpr } = expr;
-    const leftValue = evaluate(leftExpr);
-    const rightValue = evaluate(rightExpr);
-    return apply(op, leftValue, rightValue);
-  }
-  if (isVariable(expr)) {
-    throw new Error('Unbound variable');
-  }
-  if (isLetIn(expr)) {
-    const { name, headExpr, bodyExpr } = expr;
-    return evaluate(new FunCall(new Fun(name, bodyExpr), headExpr));
-  }
-  if (isFunCall(expr)) {
-    const { funExpr, argExpr } = expr;
-    const fn = evaluate(funExpr);
-    if (!isFun(fn)) throw new TypeError();
-    const { param, bodyExpr } = fn;
-    const value = evaluate(argExpr);
-    return evaluate(substitute(value, param, bodyExpr));
-  }
+  if (isOperation(expr)) return evalOperation(expr);
+  if (isVariable(expr)) throw new Error('Unbound variable');
+  if (isLetIn(expr)) return evalLetIn(expr);
+  if (isFunCall(expr)) return evalFunCall(expr);
   return expr;
 };
 
